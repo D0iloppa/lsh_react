6ad90e2c13431fa47bfdf88871c97a054d35988a
D0iloppa <kdi3939@gmail.com>
2025-11-21 14:35:43 +0900

    fix2

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},  // tailwindcss 대신 @tailwindcss/postcss 사용
     autoprefixer: {},
   },
-};
+}
