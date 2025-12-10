d30868164231da6521b8a58e1748204cd0b69867
D0iloppa <kdi3939@gmail.com>
2025-12-10 17:23:54 +0900

    init

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1509,7 +1509,7 @@ useEffect(() => {
 
 
             return  (
-              <div style={{ margin: "20px 0" }}>
+              <div style={{ margin: "20px 0", display: venueInfo?.cat_id == 1 ? '' : 'none'}}>
                   <CouponAlert key={null} 
                     nextOpenDate = {nextOpenDateValue}
                     downloaded={ downloadedSelf.length>0 }
